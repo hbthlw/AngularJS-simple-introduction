8d6b59196fd0f4e2cefb10c899fb44ef4bd85cdc
李吉特 <168610900@qq.com>
2015-06-03 22:17:22 +0800

    view,controller,scope

---
+++ public/example/controllers/example.client.controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by li on 15/6/3.
+ */
+angular.module('example').controller('ExampleController', ['$scope',
+    function($scope) {
+        $scope.name = 'MEAN Application';
+    }
+]);
